dcab196ba4047f773c9893dfe29ddb4fc4bfc4f0
Andrés Calle <andrescalleuribe@gmail.com>
2024-02-23 16:28:45 -0500

    Init front project and tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='bg-zinc-900 h-screen text-white flex items-center justify-center'>
+      <div className='bg-gray-950 p-4 w-2/5'>
+        <h1 className='text-3xl font-bold text-center my-2'>Tasks App</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
