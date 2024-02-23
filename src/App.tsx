ffda5cd24247a1e16c1860334fe709fbaa8584e0
Andrés Calle <andrescalleuribe@gmail.com>
2024-02-23 16:54:53 -0500

    Form component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import TasksList from './components/TasksList/TasksList';
+import TaskForm from './components/TaskForm/TaskForm';
+
 function App() {
   return (
     <div className='bg-zinc-900 h-screen text-white flex items-center justify-center'>
       <div className='bg-gray-950 p-4 w-2/5'>
         <h1 className='text-3xl font-bold text-center my-2'>Tasks App</h1>
+        <TaskForm />
+        <TasksList />
       </div>
     </div>
   );
---
+++ src/components/TaskForm/TaskForm.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent, useState } from 'react';
+
+function TaskForm() {
+  const [task, setTask] = useState({ title: '', description: '', done: false });
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault;
+    console.log(task);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className='border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2'
+          placeholder='Write a title'
+          type='text'
+          name='title'
+          onChange={handleInputChange}
+        />
+        <textarea
+          className='border-2 border-gray-700 p-2 rounded-lg bg-zinc-800 block w-full my-2'
+          placeholder='Write a description'
+          name='description'
+          rows={3}
+          onChange={handleInputChange}
+        ></textarea>
+        <label className='inline-flex items-center gap-x-2' htmlFor=''>
+          <input
+            className='h-5 w-5 text-indigo-600 '
+            type='checkbox'
+            onChange={(e) => setTask({ ...task, done: !task.done })}
+          />{' '}
+          <span>Done</span>
+        </label>
+        <button className='bg-indigo-500 px-3 block py-2 w-full'>Save</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
